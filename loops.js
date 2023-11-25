abb1012ba3dbe2e7aa277800e292edc7e6cfca49
kaidecoder <131601358+kaidecoder@users.noreply.github.com>
2023-11-25 14:37:36 -0500

    added loop for odd/even/prime

--- loops.js
+++ loops.js
@@ -3,64 +3,84 @@ for (let i = 0; i < 10; i++) {
 }
 
 //count down from 10 to 1
-for(let i=10; i>0; i--){
-    console.log(i)
+for (let i = 10; i > 0; i--) {
+  console.log(i);
 }
 
 //output odd numbers from 1 to 10
-for(let i=1; i<=10; i+=2){
-    console.log(i)
+for (let i = 1; i <= 10; i += 2) {
+  console.log(i);
 }
 
 //output even numbers from 1 to 10
-for(let i=0; i<=10; i+=2){
-    console.log(i)
+for (let i = 0; i <= 10; i += 2) {
+  console.log(i);
 }
 
 //multiples of 3, starting at 6 and ending at 60
-for(let i=6; i<= 60; i+=3){
-    console.log(i)
+for (let i = 6; i <= 60; i += 3) {
+  console.log(i);
 }
 
-const tree = document.querySelector(".tree")
+//loop that iterates from 1 to any number
+//print prime for primes, even for evens, odd, but treat 2 as even, and 1&3 as odd rather than prime
 
+for (let i = 1; i <= 20; i++) {
+  if (i == 2) {
+    console.log(i, "even");
+  } else if (i === 1 || i === 3) {
+    console.log(i, "odd");
+  } else {
+    if (i % 2 === 0) {
+      console.log(i, "even");
+    } else {
+      if (i % 3 == 0) {
+        console.log(i, "odd");
+      } else {
+        console.log(i, " prime");
+      }
+    }
+  }
+}
+
+const tree = document.querySelector(".tree");
 
 //make a christmas tree
-function christmasTree(){
-    for(let i=1; i<= 10; i++){
-        tree.innerHTML += ("<br>")
-        for(let j=i; j<10; j++){
-            tree.innerHTML += ("# ")
-            tree.style.textAlign = "center"
-            tree.style.fontSize = "2rem"
-            tree.style.fontWeight = "bolder"
-            tree.style.color = "green"
-        }   
+function christmasTree() {
+  for (let i = 1; i <= 10; i++) {
+    tree.innerHTML += "<br>";
+    for (let j = i; j < 10; j++) {
+      tree.innerHTML += "# ";
+      tree.style.textAlign = "center";
+      tree.style.fontSize = "2rem";
+      tree.style.fontWeight = "bolder";
+      tree.style.color = "green";
     }
+  }
 }
-christmasTree()
+christmasTree();
 
 //make the christmas star
-function christmasStar(){
-    const div = document.createElement("div")
-    const div2 = document.createElement("div")
-    const light = document.querySelector(".light")
-    const light2 = document.querySelector(".light2")
-    div.style.height = "80px"
-    div2.style.height = "80px"
-    div.style.width = "80px";
-    div2.style.width = "80px";
-    div.style.backgroundColor = "yellow"
-    div2.style.backgroundColor = "orange"
-    light.appendChild(div)
-    light2.appendChild(div2)
-    light.style.position = "absolute"
-    light2.style.position = "absolute"
-    light.style.left = "700px"
-    light2.style.left = "200px"
-    div.style.borderRadius = "50%"
-    div2.style.borderRadius = "50%"
-    div.style.border = "5px solid red"
-    div2.style.border = "5px solid red"
+function christmasStar() {
+  const div = document.createElement("div");
+  const div2 = document.createElement("div");
+  const light = document.querySelector(".light");
+  const light2 = document.querySelector(".light2");
+  div.style.height = "80px";
+  div2.style.height = "80px";
+  div.style.width = "80px";
+  div2.style.width = "80px";
+  div.style.backgroundColor = "yellow";
+  div2.style.backgroundColor = "orange";
+  light.appendChild(div);
+  light2.appendChild(div2);
+  light.style.position = "absolute";
+  light2.style.position = "absolute";
+  light.style.left = "700px";
+  light2.style.left = "200px";
+  div.style.borderRadius = "50%";
+  div2.style.borderRadius = "50%";
+  div.style.border = "5px solid red";
+  div2.style.border = "5px solid red";
 }
-christmasStar()
+christmasStar();
